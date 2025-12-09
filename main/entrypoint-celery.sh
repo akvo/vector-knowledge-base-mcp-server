f317cbf7bf79672e763ed289c5df516550ffe987
wayangalihpratama <wgprtm@gmail.com>
2025-12-09 10:32:33 +0800

    [#31] Modify entrypoint-celery

--- main/entrypoint-celery.sh
+++ main/entrypoint-celery.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -e
 
-MODE=${CELERY_MODE:-worker}
+MODE=${1:-${CELERY_MODE:-worker}}
+DATABASE_URL=${DATABASE_URL}
+
+# --- Wait for Postgres to be ready ---
+echo "⏳ Waiting for Postgres..."
+until psql "$DATABASE_URL" -c '\q' 2>/dev/null; do
+    echo "   Postgres not ready yet..."
+    sleep 2
+done
+echo "✅ Postgres is ready!"
+
 
 echo "Starting Celery in '$MODE' mode..."
-
 if [ "$MODE" = "worker" ]; then
     celery -A app.celery_app worker --loglevel=INFO
     elif [ "$MODE" = "beat" ]; then
